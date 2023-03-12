dff36c1df0f8df6e6cc03f1f744e98c43fd0bbdd
perseverance <perseverancemu@gmail.com>
2023-03-12 07:46:46 -0700

    C - Static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-		
-int _putchar(char c);		
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -21,4 +27,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+
+#endif /*_MAIN_H_ */
